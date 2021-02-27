99f00ff5031328d6aedfa80a4082c37399551dc8
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-02-27 17:08:40 +0200

    Update check.py

--- check.py
+++ check.py
@@ -7,7 +7,7 @@ import uuid
 
 DateBase = DB("DateBase/BD.pydb")
 
-def check(IP_PORT, DateBase):
+def check(IP_PORT):
 	record = {
 		"_id" : int(uuid.uuid4()),
 		"IP"  : IP_PORT[0],
@@ -16,20 +16,21 @@ def check(IP_PORT, DateBase):
 		"WORK": False,
 	}
 
+	proxies = {
+		"https": f"{IP_PORT[0]}:{IP_PORT[1]}",
+		"http": f"{IP_PORT[0]}:{IP_PORT[1]}"
+	}
+
+
 	try:
 		start = time()
-		headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-		proxies = {
-			"https": f"{IP_PORT[0]}:{IP_PORT[1]}",
-			"http": f"{IP_PORT[0]}:{IP_PORT[1]}"
-		}
-
 
 		requests.get(
 			"https://www.google.com.ru/",
 			proxies=proxies,
 			headers=headers,
 		)
+
 		record.update({"WORK": True, "TIME": time() - start})
 
 	except requests.exceptions.ProxyError:
@@ -38,7 +39,6 @@ def check(IP_PORT, DateBase):
 	DateBase.create(record)
 
 
-#for i in parsing():
-	#Thread(target=check, args=(i, DateBase)).start()
-
-print(parsing())
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+for i in parsing():
+	Thread(target=check, args=(i,)).start()
