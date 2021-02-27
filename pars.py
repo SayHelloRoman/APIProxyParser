8747d0c2aa6ac8effe07ddfd1feee8e0463b7c6e
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-02-27 17:19:25 +0200

    Update pars.py

--- pars.py
+++ pars.py
@@ -3,19 +3,19 @@ import requests
 
 
 def parsing():
-	p = BeautifulSoup(requests.get("https://www.proxynova.com/proxy-server-list").text)
-	tbody = p.find_all("tbody")[0].find_all("tr")
-	ip_port = []
+    p = BeautifulSoup(requests.get("https://www.proxynova.com/proxy-server-list").text)
+    tbody = p.find_all("tbody")[0].find_all("tr")
+    ip_port = []
 
-	for f in tbody:
-		try:
-			ip = f.find("abbr")["title"]
-			port = f.find_all("td")[1].text.strip()
-			country = f.find_all("a")[0]["title"][13:].lower()
+    for f in tbody:
+        try:
+            ip = f.find("abbr")["title"]
+            port = f.find_all("td")[1].text.strip()
+            country = f.find_all("a")[0]["title"][13:].lower()
 
-			ip_port.append([ip, port, country])
+            ip_port.append([ip, port, country])
 
-		except TypeError:
-			pass
+        except TypeError:
+            pass
 
-	return ip_port
+    return ip_port
