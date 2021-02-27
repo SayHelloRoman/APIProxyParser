29aabf0dd5cfb7fa080253e23a41ec1c7d91ef78
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-02-27 17:18:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ DateBasePROXY = DB("DateBase/BD.pydb")
 
 @app.get("/proxy_all")
 async def proxy_all():
-	return [DateBasePROXY.search(i) for i in DateBasePROXY.lists_id]
+    return [DateBasePROXY.search(i) for i in DateBasePROXY.lists_id]
 
 
 @app.get("/proxy_settings")
 async def read_root(work: Optional[str] = "None", port: Optional[str] = "None", country: Optional[str] = "None"):
-	return list(
-		filter(
-			lambda x: work in (str(x["WORK"]), "None") and port in (str(x["PORT"]), "None") and country in (str(x["COUNTRY"]), "None"),
-			(DateBasePROXY.search(i) for i in DateBasePROXY.lists_id)
-		)
-	)
+    return list(
+        filter(
+            lambda x: work in (str(x["WORK"]), "None") and port in (str(x["PORT"]), "None") and country in (str(x["COUNTRY"]), "None"),
+            (DateBasePROXY.search(i) for i in DateBasePROXY.lists_id)
+        )
+    )
